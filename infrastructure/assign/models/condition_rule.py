638b44c509f5e46b7e520d8116fdee981c38c09d
vash <phyrox.vash512@gmail.com>
2024-05-30 02:54:40 -0600

    [YD-60] metodos de evaluacion para ConditionRule en base a Member

--- infrastructure/assign/models/condition_rule.py
+++ infrastructure/assign/models/condition_rule.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models import JSONField
 
+from infrastructure.member.models.member import Member
 from infrastructure.service.models import Platform
 from infrastructure.xtra.models import Extra
 
@@ -51,3 +52,60 @@ class ConditionRule(models.Model):
     class Meta:
         verbose_name = 'Regla de condición'
         verbose_name_plural = 'Reglas de condición'
+
+    def evalue_platform(self, platform_name: str) -> bool | None:
+        platform_names = self.platforms.values_list('name', flat=True)
+        if not platform_names:
+            return None
+        return platform_name in platform_names
+
+    def evalue_circles(self, member: Member) -> bool | None:
+        condition_circles_ids = self.circles.all().values_list('id', flat=True)
+        if not condition_circles_ids:
+            return None
+
+        member_circles_ids = member.extra_values.all()\
+            .values_list('extra__id', flat=True).distinct()
+
+        match_conditions = [
+            member_cir in condition_circles_ids
+            for member_cir in member_circles_ids]
+
+        # quisas sea necesario agregar match_all_circles
+        return any(match_conditions)
+
+    def evalue_extra(self, member: Member) -> bool | None:
+        if not self.extra:
+            return None
+
+        if self.extra_exits:
+            return member.extra_values.filter(
+                extra=self.extra).exists()
+        else:
+            return member.extra_values.filter(
+                extra=self.extra, value__in=self.extra_values).exists()
+
+    def evalue_roles(self, member: Member) -> bool | None:
+        condition_roles_ids = self.roles.all().values_list('id', flat=True)
+        if not condition_roles_ids:
+            return None
+
+        return member.role_id in condition_roles_ids
+
+    def evalue(self, member: Member, platform_name: str) -> bool:
+        evalue_platform = self.evalue_platform(platform_name)
+        evalue_circles = self.evalue_circles(member)
+        evalue_extra = self.evalue_extra(member)
+        evalue_roles = self.evalue_roles(member)
+
+        match_conditions = [
+            evalue_platform, evalue_circles, evalue_extra, evalue_roles
+        ]
+
+        match_conditions = [
+            match for match in match_conditions if match is not None]
+
+        if self.match_all_conditions:
+            return all(match_conditions)
+        else:
+            return any(match_conditions)
