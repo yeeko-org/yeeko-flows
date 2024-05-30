dbcad9d3ce0cedb3eea473d643746a62e6e790c1
vash <phyrox.vash512@gmail.com>
2024-05-30 02:37:03 -0600

    [YD-61] metodos de Extra anidados al modelo Member importaciones a nivel de metodos y sin tipado para extra e interaction para evitar importacion circular

--- infrastructure/member/models/member.py
+++ infrastructure/member/models/member.py
@@ -29,3 +29,48 @@ class Member(models.Model):
         verbose_name_plural = "Integrantes"
         verbose_name = "Integrante"
 
+    # -----------------------------Extra manager-----------------------------
+
+    def add_extra_value(
+            self, extra,  value: str | None = None,
+            interaction=None,
+            origin: str = "unknown", list_by=None
+    ):
+        from infrastructure.talk.models import ExtraValue
+        if not extra:
+            return
+        extra_value, _ = ExtraValue.objects.get_or_create(
+            extra=extra,
+            member=self
+        )
+
+        extra_value.value = value
+        extra_value.origin = origin
+        extra_value.list_by = list_by  # type: ignore
+        extra_value.save()
+
+        if interaction:
+            extra_value.interactions.add(interaction)
+
+    def add_circles(
+        self, circles,
+            interaction=None,
+            origin: str = "unknown", list_by=None
+    ):
+        for circle in circles:
+            self.add_extra_value(circle, None, interaction, origin, list_by)
+
+    def remove_extra(
+            self, extra
+    ):
+        from infrastructure.talk.models import ExtraValue
+        if not extra:
+            return
+        ExtraValue.objects.filter(extra=extra, member=self).delete()
+
+    def remove_extras(self, extras):
+        from infrastructure.talk.models import ExtraValue
+        ExtraValue.objects.filter(
+            extra__in=extras,  member=self
+        ).delete()
+    # ---------------------------end Extra manager---------------------------
