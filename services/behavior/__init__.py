0fc06d4c4838586f90076d76ffad9ccf2f2e399e
vash <phyrox.vash512@gmail.com>
2024-06-05 03:48:00 -0600

    declaracion del behavior start

--- services/behavior/__init__.py
+++ services/behavior/__init__.py
@@ -1,1 +1,2 @@
 from services.behavior.reset import ResetBehavior as reset
+from services.behavior.start import StartBehavior as start
--- services/behavior/start.py
+++ services/behavior/start.py
@@ -2,15 +2,14 @@ from services.processor.behavior_processor import BehaviorProcessor
 from services.response.abstract import ResponseAbc
 
 
-class ResetBehavior:
+class StartBehavior:
     def __init__(
             self, response: ResponseAbc, default_message=None,
-            behavior_name_default="Started", **kwargs
+            behavior_name_default="start", **kwargs
     ):
-        response.message_text(
-            default_message or "Tus Configuraciones sera reiniciadas, por "
-            "favor espera un momento..."
-        )
+        if default_message:
+            response.message_text(default_message)
+
         try:
             BehaviorProcessor(behavior_name_default, response).process()
         except Exception as e:
