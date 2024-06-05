8b55d70141db4fe46708c42b459cfd6c4c73b9ca
vash <phyrox.vash512@gmail.com>
2024-06-05 03:50:49 -0600

    TextMessage y InteractiveMessage  ahora heredan de InteractionMessage, les permite tener la propiedad interaction

--- services/request/message_model.py
+++ services/request/message_model.py
@@ -1,8 +1,7 @@
 
 import time
-from typing import Optional
+from typing import Any, Optional
 
-from infrastructure.service.models import ApiRecord
 from infrastructure.talk.models import BuiltReply, Interaction
 from pydantic import BaseModel
 
@@ -10,7 +9,6 @@ from pydantic import BaseModel
 class MessageBase(BaseModel):
     message_id: str
     timestamp: int
-    api_request: Optional[ApiRecord] = None
     context_id: Optional[str] = None
 
     class Config:
@@ -18,7 +16,6 @@ class MessageBase(BaseModel):
 
     def valid_time_interval(self, is_status=False, raise_exception=True):
         max_time = 60000 if is_status else 30000
-        self.datetime = self.timestamp
         timestamp_server = int(time.time())
         long_time_interval = self.timestamp > timestamp_server + max_time
 
@@ -27,11 +24,34 @@ class MessageBase(BaseModel):
         return long_time_interval
 
 
-class TextMessage(MessageBase):
+class InteractionMessage(MessageBase):
+    interaction: Optional[Interaction] = None
+
+    def model_dump(self, *args, **kwargs) -> dict[str, Any]:
+        original_dump = super().model_dump(*args, **kwargs)
+        if self.interaction:
+            original_dump["interaction"] = str(self.interaction)
+        return original_dump
+
+    def record_interaction(self, api_record, member_account, raw_payload):
+        self.interaction = Interaction.objects.create(
+            mid=self.message_id,
+            interaction_type_id="default",
+            member_account=member_account,
+            timestamp=self.timestamp,
+            api_record_out=api_record,  # salida del servidor
+            raw_payload=raw_payload
+        )
+
+
+class TextMessage(InteractionMessage):
     text: str
 
+    def record_interaction(self, api_record, member_account):
+        return super().record_interaction(api_record, member_account, self.text)
 
-class InteractiveMessage(MessageBase):
+
+class InteractiveMessage(InteractionMessage):
     payload: str
     title: Optional[str]
     built_reply: Optional[BuiltReply] = None
@@ -41,6 +61,9 @@ class InteractiveMessage(MessageBase):
             self.built_reply = BuiltReply.objects.get(uuid=self.payload)
         except BuiltReply.DoesNotExist:
             self.built_reply = None
+
+    def record_interaction(self, api_record, member_account):
+        return super().record_interaction(api_record, member_account, self.payload)
 
 
 class EventMessage(MessageBase):
