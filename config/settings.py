1e44e77017eed1da7faf01a19f88bc447eaf8122
vash <phyrox.vash512@gmail.com>
2024-07-23 19:37:29 -0600

    configuracion de storage y app de archivos persistentes para envio de media_id

--- config/settings.py
+++ config/settings.py
@@ -1,34 +1,34 @@
 import os
-
+from os import getenv
 from dotenv import load_dotenv
 from pathlib import Path
 
-from config.utils import getenv_bool, getenv_db, getenv_list
+from config.utils import getenv_bool, getenv_db, getenv_list, getenv_int
 
 load_dotenv()
 
 
 ADMINS = (('Yeeko Report', 'ricardo@yeeko.org'), )
-APP_VERSION = os.getenv('APP_VERSION', '5.0.0')
+APP_VERSION = getenv('APP_VERSION', '5.0.0')
 BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = getenv_bool('DEBUG')
-SITE_ID = int(os.getenv('SITE_ID', '1'))
+SITE_ID = getenv_int('SITE_ID', 1)
 
 
 # Django base config
 ALLOWED_HOSTS = getenv_list('ALLOWED_HOSTS', ['*'])
 
-SECRET_KEY = os.getenv(
-    'SECRET_KEY', 'yeeko-django-insecure-*#v9ug2&ua-98ubs7u7)!uvqn*ud#ct73oh5'
+SECRET_KEY = getenv(
+    'SECRET_KEY', 'yeeko-django-insecure-secret-key'
 )
 
-AUTH_USER_MODEL = os.getenv('AUTH_USER_MODEL', 'auth.User')
+AUTH_USER_MODEL = getenv('AUTH_USER_MODEL', 'auth.User')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 APPEND_SLASH = getenv_bool('APPEND_SLASH', False)
 DATETIME_INPUT_FORMATS = ('%d-%m-%Y %H:%M:%S',)
-HTTP_X_FORWARDED_HOST = os.getenv('HTTP_X_FORWARDED_HOST', False)
+HTTP_X_FORWARDED_HOST = getenv('HTTP_X_FORWARDED_HOST', False)
 USE_X_FORWARDED_HOST = getenv_bool('USE_X_FORWARDED_HOST')
 
 ROOT_URLCONF = 'config.urls'
@@ -44,12 +44,14 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
+    'storages',
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
     'infrastructure.users.apps.UsersConfig',
     'infrastructure.service.apps.ServiceConfig',
     'infrastructure.place.apps.PlaceConfig',
+    'infrastructure.persistent_media.apps.PersistentMediaConfig',
     'infrastructure.member.apps.MemberConfig',
     'infrastructure.flow.apps.FlowConfig',
     'infrastructure.xtra.apps.XtraConfig',
@@ -62,8 +64,8 @@ INSTALLED_APPS = (
 
 
 # ----------------------------Internationalization----------------------------
-LANGUAGE_CODE = os.getenv('LANGUAGE_CODE', 'es-us')
-TIME_ZONE = os.getenv('TIME_ZONE', 'UTC')
+LANGUAGE_CODE = getenv('LANGUAGE_CODE', 'es-us')
+TIME_ZONE = getenv('TIME_ZONE', 'UTC')
 USE_I18N = getenv_bool('USE_I18N')
 USE_L10N = getenv_bool('USE_L10N')
 USE_TZ = getenv_bool('USE_TZ')
@@ -74,7 +76,7 @@ USE_TZ = getenv_bool('USE_TZ')
 POSTRGRESQL_DB = getenv_bool('POSTRGRESQL_DB', False)
 
 DATABASES = {
-    'default': getenv_db(is_postgres=POSTRGRESQL_DB, base_dir=BASE_DIR)
+    'default': getenv_db(is_postgres=POSTRGRESQL_DB)
 }
 # ---------------------------------/Data Base---------------------------------
 
@@ -89,7 +91,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
-COMPRESS_URL = os.getenv('COMPRESS_URL', '/static/')
+COMPRESS_URL = getenv('COMPRESS_URL', '/static/')
 COMPRESS_ROOT = BASE_DIR / 'static_compressed'
 COMPRESS_ENABLED = getenv_bool('COMPRESS_ENABLED')
 COMPRESS_OFFLINE = getenv_bool('COMPRESS_OFFLINE')
@@ -97,9 +99,37 @@ COMPRESS_OFFLINE = getenv_bool('COMPRESS_OFFLINE')
 STATIC_URL = COMPRESS_URL
 STATIC_ROOT = COMPRESS_ROOT
 
-MEDIA_PATH = BASE_DIR / 'media'
-MEDIA_ROOT = MEDIA_PATH
-MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
+
+S3_STORAGE_MANAGER = getenv_bool('S3_STORAGE_MANAGER', True)
+
+if S3_STORAGE_MANAGER:
+
+    AWS_ACCESS_KEY_ID = getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = getenv('AWS_SECRET_ACCESS_KEY')
+
+    AWS_STORAGE_BUCKET_NAME = getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_REGION_NAME = getenv('AWS_S3_REGION_NAME')
+    AWS_PRELOAD_METADATA = getenv_bool('AWS_PRELOAD_METADATA', True)
+    AWS_S3_CUSTOM_DOMAIN = getenv('AWS_S3_CUSTOM_DOMAIN')
+
+    # AWS_LOCATION = 'media'
+    AWS_DEFAULT_ACL = getenv('AWS_DEFAULT_ACL', 'public-read')
+
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    AWS_MEDIA_URL = getenv('AWS_MEDIA_URL', '')
+    MEDIA_URL = AWS_MEDIA_URL + "/"
+
+    AWS_S3_FILE_OVERWRITE = getenv_bool('AWS_S3_FILE_OVERWRITE', False)
+
+    CAN_DELETE_AWS_STORAGE_FILES = getenv_bool(
+        'CAN_DELETE_AWS_STORAGE_FILES', False)
+
+else:
+
+    MEDIA_PATH = BASE_DIR / 'media'
+    MEDIA_ROOT = MEDIA_PATH
+    MEDIA_URL = getenv('MEDIA_URL', '/media/')
 
 TEMPLATES = [
     {
@@ -193,7 +223,22 @@ REST_FRAMEWORK = {
 
 
 # -----------------------------------Meta-------------------------------------
-WEBHOOK_TOKEN_WHATSAPP = os.getenv('WEBHOOK_TOKEN_WHATSAPP', 'whatsapp')
+WEBHOOK_TOKEN_WHATSAPP = getenv('WEBHOOK_TOKEN_WHATSAPP', 'whatsapp')
 # -----------------------------------Meta-------------------------------------
 
-CREATE_USER_PRIVATE_HASH = os.getenv('CREATE_USER_PRIVATE_HASH', 'yeeko')
+CREATE_USER_PRIVATE_HASH = getenv('CREATE_USER_PRIVATE_HASH', 'yeeko')
+
+# ---------------------------DJANGO REST FRAMEWORK----------------------------
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
+
+# -------------------------end DJANGO REST FRAMEWORK--------------------------
