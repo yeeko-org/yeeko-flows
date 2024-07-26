af5679f252d8998158f2ee7e4eea315a203de117
vash <phyrox.vash512@gmail.com>
2024-07-26 02:12:05 -0600

    Botones por texto escrito Procesador de Reply simplificado

--- services/processor/text.py
+++ services/processor/text.py
@@ -1,13 +1,14 @@
 import re
 from typing import Optional
 
-from infrastructure.box.models import Piece, Written
+from infrastructure.box.models import Piece, Reply, Written
 from infrastructure.member.models import MemberAccount
 from infrastructure.service.models import ApiRecord
 from infrastructure.talk.models import Interaction
 from infrastructure.xtra.models import Extra
 from services.processor.behavior import BehaviorProcessor
 from services.processor.base_mixin import Processor
+from services.processor.reply import ReplyProcessor
 from services.processor.written import WrittenProcessor
 from services.request.message_model import TextMessage
 from services.response import ResponseAbc
@@ -22,6 +23,7 @@ class TextMessageProcessor(Processor):
     written: Optional[Written] = None
     default_extra: Optional[Extra] = None
     last_interaction_out: Optional[Interaction] = None
+    piece_out: Optional[Piece] = None
 
     def __init__(
             self, manager_flow, message: TextMessage, response: ResponseAbc
@@ -49,6 +51,7 @@ class TextMessageProcessor(Processor):
             if context_interaction and context_interaction.fragment:
                 context_piece = context_interaction.fragment.piece
 
+        self.piece_out = context_piece
         self.written = getattr(context_piece, "written", None)
         self.default_extra = getattr(context_piece, "default_extra", None)
 
@@ -65,6 +68,9 @@ class TextMessageProcessor(Processor):
 
         if not self.last_interaction_out:
             self.call_behavior("start")
+            return
+
+        if self.check_buttons_text():
             return
 
         valid_time_interval = self.message.valid_time_interval(
@@ -99,3 +105,38 @@ class TextMessageProcessor(Processor):
             written=self.written, default_extra=self.default_extra
         ).process()
         return True
+
+    def check_buttons_text(self):
+        # estandarizar texto, buscar normalizador de texto
+        replays = Reply.objects.filter(
+            fragment__piece=self.piece_out)
+
+        reply_by_text = None
+
+        for replay in replays:
+            if not replay.title:
+                continue
+            if standar(replay.title) == standar(self.message.text):
+                reply_by_text = replay
+                break
+
+        if not reply_by_text:
+            return False
+
+        reply_processor = ReplyProcessor(
+            reply=reply_by_text, response=self.response,
+            interaction=self.last_interaction_out
+        )
+        reply_processor.process()
+        return True
+
+
+def standar(text: str) -> str:
+    import unidecode
+    import re
+    if not text:
+        return ""
+    text = text.upper().strip()
+    text = unidecode.unidecode(text)
+    text = re.sub(r'[^a-zA-Z\s]', '', text)
+    return text.strip()
