030efc121b722d27cbc593c8b990bbfe514ae4fc
vash <phyrox.vash512@gmail.com>
2024-08-24 02:22:07 -0600

    nueva api para el envio de mensage simple y de template

--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('presentation.webhook.urls')),
+    path('api/', include('presentation.api.urls')),
 ]
