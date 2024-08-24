12a43b4c50abb0a22c15655c91d8ded8f40e5cbe
vash <phyrox.vash512@gmail.com>
2024-08-24 02:20:46 -0600

    se agrego los modelos de PlatformTemplate al admin

--- presentation/admin/box/__init__.py
+++ presentation/admin/box/__init__.py
@@ -4,3 +4,4 @@ from .piece import PieceAdmin  # noqa
 from .written import WrittenAdmin  # noqa
 from .destination import DestinationAdmin  # noqa
 from .message_link import MessageLinkAdmin  # noqa
+from .platform_template import PlatformTemplateAdmin  # noqa
