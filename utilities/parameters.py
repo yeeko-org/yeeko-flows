61b45b47758bdc328feb6d6e579513f19e6d673a
vash <phyrox.vash512@gmail.com>
2025-01-07 05:31:16 -0600

    nested_value para replace_parameter acepta nombres de variables anidadas {{key.subkey.funsion}}

--- utilities/parameters.py
+++ utilities/parameters.py
@@ -6,7 +6,7 @@ from infrastructure.assign.models import ParamValue
 
 def update_parameters(
     query_param_value: QuerySet[ParamValue],
-    parameters_dict: dict = None
+    parameters_dict: dict | None = None
 ) -> dict:
     if parameters_dict is None:
         parameters_dict = {}
@@ -19,26 +19,59 @@ def update_parameters(
 
 
 def replace_parameter(extra_values_data: dict, text: str, default: str = ""):
-    # TODO Check together: Qué pasa con las variables con puntos?
-    pattern = r"\{\{(\w+)\}\}"
+    pattern = r"\{\{([\w.]+)\}\}"
+
+    def get_nested_value(data, keys):
+        for key in keys:
+            if isinstance(data, dict) and key in data:
+                data = data[key]
+
+            elif isinstance(data, list):
+                if not data:
+                    return default
+
+                key = "0" if key == "first" else "-1" if key == "last" else key
+
+                if key.isdigit() and int(key) < len(data):
+                    data = data[int(key)]
+                elif key == "count":
+                    data = len(data)
+                elif key == "sum":
+                    data = sum(data)
+                else:
+                    return default
+
+            elif isinstance(data, str) and key in ["lower", "upper"]:
+                f_data = getattr(data, key)
+                data = f_data()
+            else:
+                return default
+        return data
 
     def replace_match(match):
-        key = match.group(1)
-        if key in extra_values_data:
-            return extra_values_data[key]
-        else:
+        variable = match.group(1)
+        keys = variable.split(".")
+        if keys[0] not in extra_values_data:
             return default
 
-    # TODO Lucian: No estamos contemplando si llega un JSON en
-    #  el valor de la variable
+        value = get_nested_value(extra_values_data[keys[0]], keys[1:])
+
+        if isinstance(value, str):
+            return value
+        elif isinstance(value, int):
+            return str(value)
+        elif isinstance(value, list):
+            return str(value[0]) if value else default
+        elif isinstance(value, dict):
+            return default
+        else:
+            return str(value)
+
     result = re.sub(pattern, replace_match, text)
-
     result_text = re.sub(r'\s+', ' ', result.strip())
-
     print("--------------------")
     print("extra_values_data: ", extra_values_data)
     print("text: ", text)
     print("result: ", result)
     print("result_text: ", result_text)
-
     return result_text
